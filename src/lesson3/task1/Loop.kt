c1427d3fffc07fd5e168a82affd1d81a66be04a0
rodion56730 <83105916+rodion56730@users.noreply.github.com>
2022-09-23 10:47:10 +0300

    1.0.0.1

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -184,7 +184,15 @@ fun lcm(m: Int, n: Int): Int {
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean = TODO()
+fun isCoPrime(m: Int, n: Int): Boolean {
+    val min1 = minOf(n, m)
+    for (i in 2..min1) {
+        if (n % i == 0 && m % i == 0) {
+            return false
+        }
+    }
+    return true
+}
 
 /**
  * Средняя (3 балла)
